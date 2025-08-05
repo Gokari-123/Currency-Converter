bc85e6e46e55c490139dacba915afc18defe4edb
Gokari-123 <mahaboobgokari@gmail.com>
2025-08-05 15:31:28 +0530

    commit changes

--- currency-converter/src/main/java/currency_converter/Controller/CurrencyController.java
+++ currency-converter/src/main/java/currency_converter/Controller/CurrencyController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/currency")
-public class CurrencyController {
+public class CurrencyController{
 
     @Autowired
     private CurrencyService currencyService;
--- currency-converter/src/main/java/currency_converter/Repository/CurrencyRateRepository.java
+++ currency-converter/src/main/java/currency_converter/Repository/CurrencyRateRepository.java
@@ -4,7 +4,6 @@ import currency_converter.Entity.CurrencyRate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Repository
